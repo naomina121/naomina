3ac6c710d1550041c85e96900a662f7ffae0d62c
naomi <naomina@me.com>
2023-04-10 14:20:24 +0900

    404レイアウトの調整

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,12 +3,8 @@ import CategoryMenu from '@/components/CategoryMenu';
 import Layout from '@/components/Layout';
 import Seo from '@/components/Seo';
 import { siteConfig } from '@/site.config';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 const CustomErrorPage = () => {
-  const router = useRouter();
-
   return (
     <Layout>
       <Seo
@@ -19,11 +15,10 @@ const CustomErrorPage = () => {
         pagePath={`${siteConfig.siteUrl}404`}
       />
       <div>
-        <CategoryMenu />
-        <div className="xl:pt-[78px] relative w-full bg-gray-200">
+        <div className="pt-[78px] relative w-full bg-gray-200">
           <div className="w-full max-w-6xl pb-10 mx-auto">
             <h1 className="xl:px-10 text-gray-800 py-10">404 Not Found</h1>
-            <p className='p-0 m-0'>お探しのページは見つかりませんでした。</p>
+            <p className="p-0 m-0">お探しのページは見つかりませんでした。</p>
           </div>
         </div>
         <Breadcrumb breadList={`404`} breadListJs={`404 Not Found`} />
