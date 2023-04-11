afeadf30b03c389759a722ef0f5cd29cbf170b71
naomi <naomina@me.com>
2023-04-11 09:08:57 +0900

    ローディングアニメーションの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Loader from '@/components/Loder';
 import '@/src/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ToastContainer, toast } from 'react-toastify';
@@ -6,6 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Loader />
       <ToastContainer />
       <Component {...pageProps} />
     </div>
