db73b1d46869f0a70bf2179d50af023cc088cb0c
naomi <naomina@me.com>
2023-09-28 18:57:47 +0900

    github

--- site.config.ts
+++ site.config.ts
@@ -3,5 +3,5 @@ export const siteConfig = {
 	description: '病気を抱えながら、挑戦する個人的学習記録',
 	siteUrl: 'https://naomina.com/',
 	twitterUrl: 'https://twitter.com/naomina_tweet',
-	githubUrl: 'https://github.com/naomina121',
+	githubUrl: 'https://github.com/naomina1201',
 };
