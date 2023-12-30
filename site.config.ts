3dd138dc644cc0a0aeac79382bcfa988f7d5253a
naomi <naomina@me.com>
2023-12-31 00:56:53 +0900

    リンクの変更

--- site.config.ts
+++ site.config.ts
@@ -2,6 +2,6 @@ export const siteConfig = {
 	title: 'NaoBlog',
 	description: 'プログラミングなどを、挑戦する個人的学習記録',
 	siteUrl: 'https://naomina.com/',
-	twitterUrl: 'https://twitter.com/naomina_tweet',
-	githubUrl: 'https://github.com/naomina1201',
+	twitterUrl: 'https://twitter.com/naomina121',
+	githubUrl: 'https://github.com/naomina121',
 };
