8ad9839ea54ea7f2c9d2cdbc98dd5abbfeaf3c78
naomi <naomina@me.com>
2024-01-08 23:30:30 +0900

    シェアボタン

--- components/post/Sns.tsx
+++ components/post/Sns.tsx
@@ -3,46 +3,46 @@ import { ContentBottomProps } from '@/types/types';
 import { getSelect, getText } from '@/utils/property';
 import React, { FC } from 'react';
 import {
-  TwitterShareButton,
-  FacebookShareButton,
-  HatenaShareButton,
-  TwitterIcon,
-  FacebookIcon,
-  HatenaIcon,
+	TwitterShareButton,
+	FacebookShareButton,
+	HatenaShareButton,
+	XIcon,
+	FacebookIcon,
+	HatenaIcon,
 } from 'react-share';
 
 const Sns: FC<ContentBottomProps> = (page) => {
-  const snsUrl =
-    siteConfig.siteUrl +
-    'study/' +
-    getSelect(page.page.properties.category.select) +
-    '/' +
-    getText(page.page.properties.slug.rich_text);
+	const snsUrl =
+		siteConfig.siteUrl +
+		'study/' +
+		getSelect(page.page.properties.category.select) +
+		'/' +
+		getText(page.page.properties.slug.rich_text);
 
-  const snsTitle = getText(page.page.properties.name.title);
+	const snsTitle = getText(page.page.properties.name.title);
 
-  return (
-    <div className="flex flex-wrap justify-between">
-      <div className="sns-button twitter">
-        <TwitterShareButton url={snsUrl} title={snsTitle}>
-          <TwitterIcon size={45} round={false} />
-          <span className="xl:hidden">Twitter</span>
-        </TwitterShareButton>
-      </div>
-      <div className="sns-button facebook">
-        <FacebookShareButton url={snsUrl} quote={snsTitle}>
-          <FacebookIcon size={45} round={false} />
-          <span className="xl:hidden">FaceBook</span>
-        </FacebookShareButton>
-      </div>
-      <div className="sns-button hatena">
-        <HatenaShareButton url={snsUrl} title={snsTitle}>
-          <HatenaIcon size={45} round={false} />
-          <span className="xl:hidden">Hatena</span>
-        </HatenaShareButton>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-wrap justify-between">
+			<div className="sns-button x">
+				<TwitterShareButton url={snsUrl} title={snsTitle}>
+					<XIcon size={45} round={false} />
+					<span className="xl:hidden">X</span>
+				</TwitterShareButton>
+			</div>
+			<div className="sns-button facebook">
+				<FacebookShareButton url={snsUrl} title={snsTitle}>
+					<FacebookIcon size={45} round={false} />
+					<span className="xl:hidden">FaceBook</span>
+				</FacebookShareButton>
+			</div>
+			<div className="sns-button hatena">
+				<HatenaShareButton url={snsUrl} title={snsTitle}>
+					<HatenaIcon size={45} round={false} />
+					<span className="xl:hidden">Hatena</span>
+				</HatenaShareButton>
+			</div>
+		</div>
+	);
 };
 
 export default Sns;
